4bbc1f51c5900825f9b4e5f5850c34d1af57fc43
mlazzell17@gmail.com <MicheSi>
2020-05-15 09:28:43 -0700

    changed some code to make it easier to understand

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -2,15 +2,19 @@ class RingBuffer:
     def __init__(self, capacity):
         self.capacity = capacity
         self.data = []
-        self.size = 0
+        self.index = 0
 
     def append(self, item):
 
         if len(self.data) == self.capacity:
-            self.data[self.size] = item
+            self.data[self.index] = item
         else:
             self.data.append(item)
-        self.size = (self.size + 1) % self.capacity
+        self.index = (self.index + 1) % self.capacity
         
     def get(self):
         return self.data
+
+# add item
+# move head and tail 1 spot
+# reset to zero
